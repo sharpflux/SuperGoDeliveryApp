af125fe428252329ba9ed21acab4426a33e83413
SharpFlux 1 <aparna.sharpflux@gmail.com>
2019-09-05 13:35:31 +0530

    usertype

--- app/src/main/java/com/sharpflux/supergodeliveryapp/ForgotPasswordActivity.java
+++ app/src/main/java/com/sharpflux/supergodeliveryapp/ForgotPasswordActivity.java
@@ -8,8 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
