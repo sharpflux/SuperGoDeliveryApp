6c2fc8dcf48108fa6870c959bef423a7df429675
SharpFlux 1 <aparna.sharpflux@gmail.com>
2020-01-22 15:09:52 +0530

    final 6

--- app/src/main/java/com/sharpflux/supergodeliveryapp/OtpRegisterActivity.java
+++ app/src/main/java/com/sharpflux/supergodeliveryapp/OtpRegisterActivity.java
@@ -47,7 +47,7 @@ public class OtpRegisterActivity extends AppCompatActivity {
         verifyButton = findViewById(R.id.verifyButton);
         resendOtpTv = findViewById(R.id.resendOtpTv);
         enterOTPCodeEt = findViewById(R.id.enterOTPCodeEt);
-        tv_timer1 = findViewById(R.id.tv_timer1);
+        tv_timer1 = findViewById(R.id.tv_timerOtp);
 
         myCountDownTimer1 = new MyCountDownTimer1(59000, 1000);
         myCountDownTimer1.start();
